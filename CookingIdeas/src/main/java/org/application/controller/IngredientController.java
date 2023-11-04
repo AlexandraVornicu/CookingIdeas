79f3e5d75af8d91a68cbb206628210c94b9ebb25
AlexandraVornicu <alexandra.i.vornicu@gmail.com>
2023-11-04 19:11:34 +0200

    refactored POST controller to show multiple validation messages

--- CookingIdeas/src/main/java/org/application/controller/IngredientController.java
+++ CookingIdeas/src/main/java/org/application/controller/IngredientController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import org.application.service.IngredientService;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class IngredientController {
@@ -32,16 +34,24 @@ public class IngredientController {
         CustomResponseDTO customResponseDTO = new CustomResponseDTO();
 
         if (bindingResult.hasErrors()) {
-            String errorMessage = bindingResult.getFieldError().getDefaultMessage();
+            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+
+            StringBuilder errorMessages = new StringBuilder();
+            for (FieldError errorMessage : fieldErrors) {
+                errorMessages.append(errorMessage.getDefaultMessage()).append(" ");
+            }
+
             customResponseDTO.setResponseObject(null);
-            customResponseDTO.setResponseMessage(errorMessage);
+            customResponseDTO.setResponseMessage(String.valueOf(errorMessages));
+
             return new ResponseEntity<>(customResponseDTO, HttpStatus.BAD_REQUEST);
         }
+
         ingredientService.createIngredient(ingredientDTO);
         customResponseDTO.setResponseObject(ingredientDTO);
         ingredientDTOList.add(ingredientDTO);
         customResponseDTO.setResponseMessage("Ingredient creat cu succes.");
         return new ResponseEntity<>(customResponseDTO, HttpStatus.CREATED);
-    }
 
+    }
 }
